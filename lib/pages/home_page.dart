9ee56149de47ffb6d60043b832d11019f6e5ba9d
Waliia <waliazhar45@gmail.com>
2024-02-26 12:50:02 +0500

    added drawer on the homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -21,10 +21,10 @@ class _HomePageState extends State<HomePage> {
 
   final List<Widget> _pages = [
     // Shop page
-    MyShopPage(),
+    const MyShopPage(),
 
     // Cart page
-    MyCart(),
+    const MyCart(),
   ];
 
   @override
@@ -33,6 +33,86 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: Theme.of(context).primaryColor,
       bottomNavigationBar: MyBottomNavBar(
         onTabChange: (index) => navigateBottomBar(index),
+      ),
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        leading: Builder(builder: (context) {
+          return IconButton(
+            onPressed: () => Scaffold.of(context).openDrawer(),
+            icon: const Padding(
+              padding: EdgeInsets.only(left: 12),
+              child: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+            ),
+          );
+        }),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey.shade900,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                DrawerHeader(
+                  child: Image.asset(
+                    width: 100,
+                    'images/nike.png',
+                    color: Colors.white,
+                  ),
+                ),
+                // Padding(
+                //   padding: EdgeInsets.symmetric(horizontal: 24),
+                //   child: Divider(
+                //     color: Colors.grey.shade800,
+                //   ),
+                // ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 24),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.home,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'Home',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 24),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.info,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'About',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 24, bottom: 24),
+              child: ListTile(
+                leading: Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  'Logout',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
       body: _pages[_selectedIndex],
     );
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -11,7 +11,7 @@ class _MyShopPageState extends State<MyShopPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Theme.of(context).primaryColor,
     );
   }
 }
